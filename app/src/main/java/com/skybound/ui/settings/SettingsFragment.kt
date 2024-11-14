6406b16b2a0f6dd8e8b0d65e915e80e982af0f24
ngodeing <onlytaufik47@gmail.com>
2024-11-14 19:45:44 +0700

    add dark mode feature and adjust color resource

--- app/src/main/java/com/skybound/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/skybound/ui/settings/SettingsFragment.kt
@@ -4,34 +4,41 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.skybound.databinding.FragmentSettingsBinding
+import com.skybound.ui.utils.ViewModelFactory
 
 class SettingsFragment : Fragment() {
 
     private var _binding: FragmentSettingsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var settingsViewModel: SettingsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(SettingsViewModel::class.java)
-
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        val pref = SettingPreferences.getInstance(requireContext().applicationContext.dataStore)
+        settingsViewModel = ViewModelProvider(this, ViewModelFactory(pref)).get(SettingsViewModel::class.java)
+
+        settingsViewModel.getThemeSettings().observe(viewLifecycleOwner) { isDarkModeActive ->
+            binding.switchTheme.isChecked = isDarkModeActive
+            AppCompatDelegate.setDefaultNightMode(
+                if (isDarkModeActive) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO
+            )
         }
+
+        binding.switchTheme.setOnCheckedChangeListener { _, isChecked ->
+            settingsViewModel.saveThemeSetting(isChecked)
+        }
+
         return root
     }
 
