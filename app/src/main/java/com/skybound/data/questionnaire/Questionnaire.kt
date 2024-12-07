8d819c43bfd2a5da482e9487576424af0855b16a
Arkyy85 <hananfadhila@gmail.com>
2024-12-08 01:26:49 +0700

    Adding question,answer, and score calculation on questionnaire

--- app/src/main/java/com/skybound/data/questionnaire/Questionnaire.kt
+++ app/src/main/java/com/skybound/data/questionnaire/Questionnaire.kt
@@ -1,14 +1,17 @@
 package com.skybound.data.questionnaire
 
+import java.io.Serializable
+
 data class Questionnaire(
+    val id: Int,
     val questionText: String,
     val options: List<Option>,
     val isImageQuestion: Boolean = false,
     val questionImageUrl: String? = null
-)
+) : Serializable
 
 data class Option(
-    val answerText: String? = null,
+    val answerText: String,
     val answerImageUrl: String? = null,
     val weight: Int
-)
+) : Serializable
